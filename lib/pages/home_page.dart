3ddabad85bcc8ee705b7e7dcc377961448a2d3f8
yohanessahrul <yohanessahrul92@gmail.com>
2023-08-14 19:28:55 +0700

    add introduction pages

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Yosa XX1"),
+      ),
+      body: Center(
+        child: Text("HomePage"),
+      ),
+    );
+  }
+}
---
+++ lib/pages/introduction_pages.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import './home_page.dart';
+
+class IntoructionPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return IntroductionScreen(
+      pages: [
+        PageViewModel(
+          title: "Title of introduction page",
+          body: "Welcome to the app! This is a description of how it works.",
+          image: const Center(
+            child: Icon(Icons.waving_hand, size: 50.0),
+          ),
+        ),
+        PageViewModel(
+          title: "Title of introduction page",
+          body: "Welcome to the app! This is a description of how it works.",
+          image: const Center(
+            child: Icon(Icons.waving_hand, size: 50.0),
+          ),
+        )
+      ],
+      showNextButton: true,
+      next: Text("Next"),
+      done: const Text("Done"),
+      onDone: () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HomePage(),
+          ),
+        );
+      },
+    );
+  }
+}
